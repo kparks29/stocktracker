83c47bb4a7f605b079c39e8d9ff4adfc28ea96ec
Nathaniel Walston <gojiradestroy@gmail.com>
2015-09-22 22:27:46 -0700

    Removed everything from dashboard.html except the header and footer (as of yet remaining empty). The user’s full stock list is now housed in userStockList.html, and symbol analytics are housed in symbol.html.

--- src/app/dashboard/dashboard.js
+++ src/app/dashboard/dashboard.js
@@ -7,19 +7,39 @@
 				url: '/dash',
 				templateUrl: 'app/dashboard/dashboard.html',
 				controller: 'DashboardCtrl as dash'
+			})
+			.state('dashboard.userStockList', {
+				url: '/list',
+				templateUrl: 'app/dashboard/userStockList.html',
+				controller: 'UserStockListCtrl as stocks'
+			})
+			.state('dashboard.symbol', {
+				url: '/:symbol',
+				templateUrl: 'app/dashboard/symbol.html',
+				controller: 'SymbolCtrl as symbol'
 			});
-		}
+	}
 	
-	function DashboardCtrl (StockService) {
+	function DashboardCtrl () {
+
+	}
+
+	function UserStockListCtrl (StockService) {
 		var self = this;
 		StockService.getStocks().then(function (stocks){
 			self.stocks = stocks;
 		});
+	}
 
+	function SymbolCtrl ($stateParams) {
+		var self = this;
+		self.symbol = $stateParams.symbol
 	}
 
 	angular.module('StockTracker.dashboard', [] )
 		.config(config)
-		.controller('DashboardCtrl', DashboardCtrl);
+		.controller('DashboardCtrl', DashboardCtrl)
+		.controller('UserStockListCtrl', UserStockListCtrl)
+		.controller('SymbolCtrl', SymbolCtrl);
 
 })();
